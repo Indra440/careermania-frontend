6b20e9ba0f5d6aab7db747e97c1a0d784f94203e
Indranil <indranilshit440@gmail.com>
2020-11-11 20:50:08 +0530

    last change v1.5

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
-    $("#background_image").load("background.html");
-    $(".headnavbar").load("header.html");
+    $("#background_image").load("./html/background.html");
+    $(".headnavbar").load("./html/header.html");
             var count = 1;
             window.setInterval(function(){
                 buttonChange();
